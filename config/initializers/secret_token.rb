2fceab58a41c0f5c916762c9997fc9b7224273d8
Eric Oestrich <eric@oestrich.org>
2013-12-03 15:46:12 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smartchat::Application.config.secret_key_base = '4eb25fe170f86225cde8ade42fc601e7f73fda5f0f26db007b2b691147ced93e7beb2fd27e3c4cd726175bfd7850226cfbdccef7e4042a698b284d684bfaeed1'
