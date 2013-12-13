fb6e8c74ee933da30f035b9135374fbf0b7fc470
Eric Oestrich <eric@oestrich.org>
2013-12-13 15:10:40 -0500

    Setup for ec2 servers

--- chef-repo/cookbooks/smartchat/recipes/default.rb
+++ chef-repo/cookbooks/smartchat/recipes/default.rb
@@ -5,3 +5,5 @@ end
 rbenv_gem "bundler" do
   ruby_version "2.0.0-p247"
 end
+
+package "postgresql-server-dev-9.1"
---
+++ config/deploy/production.rb
@@ -0,0 +1,2 @@
+server "ec2-54-237-60-98.compute-1.amazonaws.com", :web, :app, :db, :primary => true
+set :rails_env, "production"
