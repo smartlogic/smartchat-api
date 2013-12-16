44967c297b7b95bbfe2c970afb10c095cf6f877b
Eric Oestrich <eric@oestrich.org>
2013-12-16 11:48:46 -0500

    Remove extracted code from main rails app

--- config/initializers/container.rb
+++ config/initializers/container.rb
@@ -14,21 +14,5 @@ class AppContainer
     let(:sqs_queue) do
       AWS::SQS.new.queues.named(sqs_queue_name)
     end
-
-    let(:s3_bucket_name) do
-      "smartchat-#{Rails.env}"
-    end
-
-    let(:s3_bucket) do
-      AWS::S3.new.buckets[s3_bucket_name]
-    end
-
-    let(:s3_host) do
-      "http://s3.amazon.com/#{s3_bucket_name}/"
-    end
-
-    let(:android_notifier) do
-      AndroidNotifier
-    end
   end
 end
