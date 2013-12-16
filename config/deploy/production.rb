823a572c6f79398b782eff0733b41db4b5adcdc3
Eric Oestrich <eric@oestrich.org>
2013-12-16 15:15:50 -0500

    Load production servers from ec2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,14 @@
-server "ec2-54-237-60-98.compute-1.amazonaws.com", :web, :app, :db, :primary => true
+require 'aws-sdk'
+
 set :rails_env, "production"
+
+ec2 = AWS::EC2.new
+ec2.instances.tagged("Type").tagged_values("web").each do |instance|
+  next unless instance.status == :running
+  server instance.public_dns_name, :web, :app
+end
+
+ec2.instances.tagged("Type").tagged_values("worker").each do |instance|
+  next unless instance.status == :running
+  server instance.public_dns_name, :worker, :app
+end
