f3b0869c81104ab0ab390b074cd8fd0402884e6f
Eric Oestrich <eric@oestrich.org>
2013-12-16 10:29:50 -0500

    Have fog use a separate bucket and access keys directly

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,13 @@
-fog_config = {
-  "aws_access_key_id"      => ENV['S3_ACCESS_KEY_ID'],
-  "aws_secret_access_key"  => ENV['S3_SECRET_ACCESS_KEY']
-}
-
 CarrierWave.configure do |config|
   config.storage = :fog
 
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => fog_config["aws_access_key_id"],
-    :aws_secret_access_key  => fog_config["aws_secret_access_key"],
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
   }
 
-  config.fog_directory  = "smartchat-#{Rails.env}"
+  config.fog_directory  = "smartchat-private-#{Rails.env}"
 end
 
 if Rails.env.development? || Rails.env.test?
