b5aeb0c6b09e7ba197a68f2f34d5f61547ee162b
Eric Oestrich <eric@oestrich.org>
2013-12-19 15:36:09 -0500

    Setup rsyslog to send logs to papertrail

--- chef-repo/cookbooks/smartchat/recipes/default.rb
+++ chef-repo/cookbooks/smartchat/recipes/default.rb
@@ -9,3 +9,23 @@ rbenv_gem "bundler" do
 end
 
 package "postgresql-server-dev-9.1"
+
+cookbook_file "/etc/syslog.papertrail.crt" do
+  source "syslog.papertrail.crt"
+  owner "root"
+  group "root"
+  mode 00644
+end
+
+cookbook_file "/etc/rsyslog.conf" do
+  source "rsyslog.conf"
+  owner "root"
+  group "root"
+  mode 00644
+end
+
+package "rsyslog-gnutls"
+
+service "rsyslog" do
+  action :restart
+end
