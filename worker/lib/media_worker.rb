0308d53b7463bab5bc60fa731b74638ab50b880b
Eric Oestrich <eric@oestrich.org>
2013-12-24 13:48:49 -0500

    Extract #publish private method for MediaWorker

--- worker/lib/media_worker.rb
+++ worker/lib/media_worker.rb
@@ -11,31 +11,39 @@ class MediaWorker
 
     id = media_attributes["id"]
     user_id = media_attributes["user_id"]
-    file_path = media_attributes["file_path"]
-
     encryptor = container.smartchat_encryptor.new(media_attributes["public_key"])
+    media_store = container.media_store
+
+    file_path = media_attributes["file_path"]
     s3_file_path, encrypted_aes_key, encrypted_aes_iv =
-      container.media_store.publish(file_path, user_id, id, encryptor)
+      publish(file_path, user_id, id, encryptor, media_store)
 
     notification_hash = notification_hash.merge({
       "s3_file_path" => s3_file_path,
-      "encrypted_aes_key" => Base64.strict_encode64(encrypted_aes_key),
-      "encrypted_aes_iv" => Base64.strict_encode64(encrypted_aes_iv),
+      "encrypted_aes_key" => encrypted_aes_key,
+      "encrypted_aes_iv" => encrypted_aes_iv
     })
 
     if media_attributes["drawing_path"]
       drawing_file_path = media_attributes["drawing_path"]
 
       drawing_s3_file_path, drawing_encrypted_aes_key, drawing_encrypted_aes_iv =
-        container.media_store.publish(drawing_file_path, user_id, id, encryptor)
+        publish(drawing_file_path, user_id, id, encryptor, media_store)
 
       notification_hash = notification_hash.merge({
         "drawing_s3_file_path" => drawing_s3_file_path,
-        "drawing_encrypted_aes_key" => Base64.strict_encode64(drawing_encrypted_aes_key),
-        "drawing_encrypted_aes_iv" => Base64.strict_encode64(drawing_encrypted_aes_iv)
+        "drawing_encrypted_aes_key" => drawing_encrypted_aes_key,
+        "drawing_encrypted_aes_iv" => drawing_encrypted_aes_iv
       })
     end
 
     container.notification_service.send_notification_to_devices(notification_hash)
+  end
+
+  private
+
+  def publish(path, user_id, id, encryptor, media_store)
+    public_path, encrypted_aes_key, encrypted_aes_iv = media_store.publish(path, user_id, id, encryptor)
+    [public_path, Base64.strict_encode64(encrypted_aes_key), Base64.strict_encode64(encrypted_aes_iv)]
   end
 end
