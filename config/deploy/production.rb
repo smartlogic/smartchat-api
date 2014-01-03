3536c3b78fc679bc758e4a527e0da12877127645
Eric Oestrich <eric@oestrich.org>
2014-01-03 14:08:24 -0500

    Set the first Web ec2 instance as primary db for migrations

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,10 +2,14 @@ require 'aws-sdk'
 
 set :rails_env, "production"
 
+primary = true
+
 ec2 = AWS::EC2.new
 ec2.instances.tagged("Type").tagged_values("web").each do |instance|
   next unless instance.status == :running
-  server instance.public_dns_name, :web, :app
+  server instance.public_dns_name, :web, :app, :db, :primary => primary
+
+  primary = false
 end
 
 ec2.instances.tagged("Type").tagged_values("worker").each do |instance|
