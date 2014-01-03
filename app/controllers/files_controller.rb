7daddd2af76b57fd664b1332c9a3acceff71e935
Eric Oestrich <eric@oestrich.org>
2014-01-03 15:13:33 -0500

    Remove file path generation in FilesController#show
    This can be removed because the router no longer tries to capture the
    format.

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,21 +1,11 @@
 class FilesController < ApplicationController
   def show
-    data = AppContainer.media_store.read_once(file_path)
+    data = AppContainer.media_store.read_once(params[:file_path])
 
     if data
       send_data data
     else
       head 404
-    end
-  end
-
-  private
-
-  def file_path
-    if params[:format]
-      "#{params[:file_path]}.#{params[:format]}"
-    else
-      params[:file_path]
     end
   end
 end
