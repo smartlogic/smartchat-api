723e5c2783f5e1a0f4d8c7c3a1593f76e0199e46
Eric Oestrich <eric@oestrich.org>
2014-01-06 18:02:20 -0500

    Use Redis' BRPOP to block when poping
    Removes the need to sleep

--- worker/lib/redis_queue.rb
+++ worker/lib/redis_queue.rb
@@ -9,22 +9,12 @@ class RedisQueue
 
   def poll(&block)
     loop do
-      if queue_size > 0
-        message = @redis.rpop("smartchat-queue")
-        block.call(RedisMessage.new(message))
-      end
-
-      sleep 1
+      _, message = @redis.brpop("smartchat-queue")
+      block.call(RedisMessage.new(message))
     end
   end
 
   def send_message(message_json)
     @redis.lpush("smartchat-queue", message_json)
-  end
-
-  private
-
-  def queue_size
-    @redis.llen("smartchat-queue")
   end
 end
