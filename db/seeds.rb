216408982277f138488ea3b28a151210e2ad410e
Eric Oestrich <eric@oestrich.org>
2014-01-28 17:53:05 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+["eric", "sam", "yair", "paul"].each do |user|
+  UserService.create({
+    :username => user,
+    :email => "#{user}@example.com",
+    :password => "password"
+  })
+end
+
+User.all.each do |user|
+  User.all.each do |friend|
+    FriendService.create(user, friend)
+  end
+end
