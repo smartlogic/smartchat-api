125ff9e8948c77bd04e5daae65824a157f65423f
Eric Oestrich <eric@oestrich.org>
2014-02-20 15:02:56 -0500

    Move chef into its own repo

--- chef-repo/cookbooks/smartchat/recipes/database.rb
+++
@@ -1,4 +0,0 @@
-postgresql_database "smartchat_prod" do
-  connection ({:host => "127.0.0.1", :port => 5432, :username => 'postgres', :password => node['postgresql']['password']['postgres']})
-  action :create
-end
