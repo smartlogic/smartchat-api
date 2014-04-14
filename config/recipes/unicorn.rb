3eaae5f64ab4fa37d15f574b11ccc6010d4eba24
Eric Oestrich <eric@oestrich.org>
2014-04-14 15:47:51 -0400

    Updates for the all env

--- config/recipes/unicorn.rb
+++ config/recipes/unicorn.rb
@@ -3,7 +3,11 @@ set_default(:unicorn_pid) { "#{current_path}/tmp/pids/unicorn.pid" }
 set_default(:unicorn_config) { "#{shared_path}/config/unicorn.rb" }
 set_default(:unicorn_log) { "#{shared_path}/log/unicorn.log" }
 set_default(:unicorn_workers) {
-  5
+  if Rails.env.all?
+    2
+  else
+    5
+  end
 }
 set_default(:unicorn_timeout, 30)
 
