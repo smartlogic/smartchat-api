26ac01e1e76f7fe54f66f75eebb0ea214ff9c669
Eric Oestrich <eric@oestrich.org>
2014-04-14 15:54:02 -0400

    Use environments to override the all environments unicorn worker count

--- config/deploy/all.rb
+++ config/deploy/all.rb
@@ -1,3 +1,4 @@
 server "smartchat_all", :web, :worker, :scheduler, :app, :db, :primary => true
 set :rails_env, 'all'
 set :media_workers, 1
+set :unicorn_workers, 2
--- config/recipes/unicorn.rb
+++ config/recipes/unicorn.rb
@@ -3,11 +3,7 @@ set_default(:unicorn_pid) { "#{current_path}/tmp/pids/unicorn.pid" }
 set_default(:unicorn_config) { "#{shared_path}/config/unicorn.rb" }
 set_default(:unicorn_log) { "#{shared_path}/log/unicorn.log" }
 set_default(:unicorn_workers) {
-  if Rails.env.all?
-    2
-  else
-    5
-  end
+  5
 }
 set_default(:unicorn_timeout, 30)
 
